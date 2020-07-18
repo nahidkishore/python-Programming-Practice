378974fea11a7a52e6a701da9666a3d767abc9b0
nahid <nahidkishore99@gmail.com>
2020-07-18 22:23:38 +0600

    file write, append, create

---
+++ writeFile.py
@@ -0,0 +1,8 @@
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!  okay")
+f.close()
+
+#create file
+f = open("demofile3.txt", "w")
+f.write("Now the file has more content!  okay vai thank you")
+f.close()
