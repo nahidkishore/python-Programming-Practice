e44719e3e92ea282fc3a55466108b52ca18ecfed
nahid <nahidkishore99@gmail.com>
2020-07-19 17:34:36 +0600

    method overriding

--- OOP/methodOverriding.py
+++ OOP/methodOverriding.py
@@ -3,7 +3,27 @@ class Phone:
     print('I am in a phone class')
     
 class Samsung(Phone):
-  pass
+  pass  
+#Note: Use the pass keyword when you do not want to add any other properties or methods to the class.
 
 
-s=Sumsung()
+s=Samsung()
+
+
+#Method Overriding
+
+class Phone:
+  def __init__(self):
+    print('I am in a phone class')
+    
+class Samsung(Phone):
+  def __init__(self):
+    super(). __init__()  #jodi phone class and samsung class 2ta e print korate chai tahole " super(). _init_" use korbo
+    print('I am in a samsung class')
+  
+ 
+
+
+s=Samsung()
+
+
