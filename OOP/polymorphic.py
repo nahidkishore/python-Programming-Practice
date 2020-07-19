eea71aafe8b26109b970b60d291b611d16bbc23f
nahid <nahidkishore99@gmail.com>
2020-07-19 21:52:52 +0600

    polymorphism metho part-1

---
+++ OOP/polymorphic.py
@@ -0,0 +1,27 @@
+#Example of inbuilt polymorphic functions :
+
+# Python program to demonstrate in-built poly- 
+# morphic functions 
+
+# len() being used for a string 
+print(len("geeks")) 
+
+# len() being used for a list 
+print(len([10, 20, 30])) 
+
+
+#Examples of used defined polymorphic functions :
+
+
+# A simple Python function to demonstrate 
+# Polymorphism 
+
+def add(x, y, z = 0): 
+	return x + y+z 
+
+# Driver code 
+print(add(2, 3)) 
+print(add(2, 3, 4)) 
+
+
+#https://www.geeksforgeeks.org/polymorphism-in-python/
